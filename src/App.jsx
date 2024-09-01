69e07ff5b2e06031bacfec0dc463efc2368431a6
ZunairaFatima <zunairafatima.dev@gmail.com>
2024-09-01 06:06:01 +0500

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./components/Navbar";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Projects from './components/Projects'
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -32,3 +32,7 @@ function App() {
 }
 
 export default App;
+
+
+
+
