a02817a5da2a66608718ca3ddc3b5203e45ada25
ZunairaFatima <zunairafatima.dev@gmail.com>
2024-09-01 05:56:54 +0500

    fixed an error.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
-import About from "./components/about";
+import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Projects from './components/Projects'
-import Contact from './components/Contact'
+import Contact from "./components/contact";
 
 function App() {
   return (
